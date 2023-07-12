3f35700078f34391ac1179c92300ca1108381da7
walter <walteroscarquintana456@gmail.com>
2023-07-12 19:46:49 -0300

    cascada

---
+++ cascada.php
@@ -0,0 +1,9 @@
+<?php
+
+echo 'Fases de modelo cascada:';
+echo 'Requerimientos: Se toma nota de las peticiones del cliente';
+echo 'Diseño: Se arma la estructura interna del software';
+echo 'Implementación: Se implementa la estructura anteriormente diseñada';
+echo 'Verificación: Se verifica que todo funcione correctamente';
+echo 'Mantenimiento';
+
