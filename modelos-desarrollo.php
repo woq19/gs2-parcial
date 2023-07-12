94eb3941a3d01336d88c011f054bfc1e336724ad
walter <walteroscarquintana456@gmail.com>
2023-07-12 19:58:59 -0300

    modelos-desarrollo

---
+++ modelos-desarrollo.php
@@ -0,0 +1,8 @@
+<?php
+
+echo 'Modelos de desarrollo:';
+echo 'modelo en V: En similar al desarrollo en cascada pero al final de cada paso se evalúa';
+echo 'Modelo espiral ';
+echo 'Modelo BigBang: Se codea y si van surgiendo problemas se corrigen sobre la marcha';
+echo 'Modelo repetitivo';
+echo 'Modelo ejecucion y corrección';
